853df0ebfb5f1820b672c5daa619b3f3a117d4fd
Madison <mlinkenmeyer@gmail.com>
2023-08-01 15:14:14 -0700

    deleted a duplicat function block

--- index.js
+++ index.js
@@ -27,25 +27,6 @@ function calculatesFarePrice(start, destination) {
     farePrice = 0;
   } else if (feetTravelled < 2000) {
     farePrice = 0.02 * (feetTravelled - 400);
-  } else if (feetTravelled > 2000) {
-    farePrice = 25;
-  } else {
-    farePrice = "cannot travel that far";
-  }
-
-  return farePrice;
-
-  //returns the fare for the customer
-}
-
-function calculatesFarePrice(start, destination) {
-  const feetTravelled = distanceTravelledInFeet(start, destination);
-  let farePrice;
-
-  if (feetTravelled < 400) {
-    farePrice = 0;
-  } else if (feetTravelled < 2000) {
-    farePrice = 0.02 * (feetTravelled - 400);
   } else if (feetTravelled < 2500) {
     farePrice = 25;
   } else {
